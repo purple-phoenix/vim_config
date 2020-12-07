a9052a684d8e63689834532faf6d06ebc6ea0f09
Matt McCarthy <matttmccarthy66@gmail.com>
2020-12-07 14:56:00 -0500

    Ensure pathogen is set

--- set_vim.sh
+++ set_vim.sh
@@ -1,2 +1,5 @@
+mkdir -p ~/.config/nvim/autoload
+wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim -O ~/.config/nvim/autoload/pathogen.vim
+
 git submodule update --init --recursive
 cp ./init.vim $HOME/.config/nvim/init.vim
